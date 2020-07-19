54c94ef71b8d3481e2ef12fcfc2504666268d945
damiano.barbati <damiano.barbati@gmail.com>
2020-07-19 18:24:37 +0200

    done

---
+++ test/test.js
@@ -0,0 +1,9 @@
+console.log('hello');
+console.log('hello', 'world');
+console.log(['hello', 'world']);
+console.log({ hello: 'world' });
+console.log(['hello', 'world'], 'yeah');
+console.log({ hello: 'world' }, 'yeah');
+console.log({ time: 'yo' });
+console.error('hello');
+console.error(new Error('ops'));
